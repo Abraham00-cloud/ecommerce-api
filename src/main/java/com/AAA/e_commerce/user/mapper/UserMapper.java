4bcfa2749046e8552d8b6744777070ce816ba26d
abraham00-cloud <alagbeabraham00@gmail.com>
2026-03-22 21:45:26 +0100

    user end update

---
+++ src/main/java/com/AAA/e_commerce/user/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.AAA.e_commerce.user.mapper;
+
+import com.AAA.e_commerce.user.dto.RegisterRequestDto;
+import com.AAA.e_commerce.user.dto.UserResponseDto;
+import com.AAA.e_commerce.user.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapper {
+    public User toUser(RegisterRequestDto requestDto) {
+        User user = new User();
+        user.setEmail(requestDto.email());
+        user.setFirstName(requestDto.firstName());
+        user.setLastName(requestDto.lastName());
+        user.setPassword(requestDto.password());
+        return user;
+    }
+
+    public UserResponseDto toUserResponseDto(User user) {
+        return new UserResponseDto(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getCart().getId());
+    }
+
+
+}
---
+++ src/main/java/com/AAA/e_commerce/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.AAA.e_commerce.user.repository;
+
+import com.AAA.e_commerce.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
